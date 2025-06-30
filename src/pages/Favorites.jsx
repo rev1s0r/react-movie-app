9bca164b2a22fa7d2df81de5ab86de75b1617225
rev1s0r <petrik.baca@gmail.com>
2025-06-30 17:18:55 +0200

    druhy commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites () {
+    return (
+        <div className="favorites-empty">
+            <h2>No favorite movies yet</h2>
+            <p>start adding favorites to see them here</p>
+        </div>
+    );
+}
+
+export default Favorites
